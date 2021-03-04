12ccf7f50bd666286f779bdfdf0030add171fe3d
laylarodas <rodas.layla@gmail.com>
2021-03-04 03:17:08 -0300

    modificacion del archivo app.js

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const path = require('path');
 const app = express();
 
 const mainRoutes = require('./routes/main');
+const productRoutes = require('./routes/products');
+const userRoutes = require('./routes/user');
 
 const publicPath = path.resolve(__dirname, './public');
 
@@ -12,14 +14,19 @@ app.set('view engine','ejs');
 
 app.use(express.static(publicPath));
 
-app.listen(4000, ()=>{
-    console.log('servidor corriendo en el puerto 4000');
+app.listen(process.env.PORT || 4000, ()=>{
+    console.log('Servidor corriendo en el puerto 4000');
 });
 
 //rutas estaticas
 app.use('/', mainRoutes);
 
+app.use('/', productRoutes);
 
+app.use('/', userRoutes);
+
+
+/*
 app.get('/', (req,res)=>{
     res.sendFile(path.resolve(__dirname, './views/index.html'));
 });
@@ -40,3 +47,5 @@ app.get('/register', (req,res)=>{
 app.get('/login', (req,res)=>{
     res.sendFile(path.resolve(__dirname, './views/login.html'));
 });
+
+*/
